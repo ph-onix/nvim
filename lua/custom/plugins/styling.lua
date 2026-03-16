e336fae74d7605da439888bbca355c459d0df5fc
ph-dos <pmiller0706@gmail.com>
2026-03-16 12:54:54 -0500

    save

--- lua/custom/plugins/styling.lua
+++ lua/custom/plugins/styling.lua
@@ -15,6 +15,9 @@ return {
             -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", fg = "NONE" })
             -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", fg = "NONE" })
 
+            -- String
+            -- vim.api.nvim_set_hl(0, "@string", { fg = "#9e5c49" })
+
             vim.api.nvim_set_hl(0, "TelescopeMatching", { bold = true, italic = false, underline = true })
             vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#FFAA88" })
             vim.api.nvim_set_hl(0, "MatchParen", { fg = "#ffffff", bold = true })
