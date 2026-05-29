7eeb3549ef74ab439476a6f13ee3c71cc32b4f98
ph-dos <pmiller0706@gmail.com>
2026-05-29 14:04:08 -0500

    feat: add auto pairs exclude parens

--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -44,4 +44,13 @@ return {
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         opts = {},
     },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            local npairs = require("nvim-autopairs")
+            npairs.setup({})
+            npairs.remove_rule("(")
+        end,
+    },
 }
